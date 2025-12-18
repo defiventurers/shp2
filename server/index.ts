70d26878bfa682289e5d745679736868c9b45413
defiventurers <140104441+defiventurers@users.noreply.github.com>
2025-12-18 07:45:39 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { serveStatic } from "./static";
 import { seedDatabase } from "./seed";
 
 const app = express();
