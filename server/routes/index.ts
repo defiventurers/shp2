33b2022efd380bdff1ed2791fae11a0762ceed1b
defiventurers <140104441+defiventurers@users.noreply.github.com>
2025-12-18 08:24:24 +0530

    Update index.ts

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,1 +1,12 @@
+import type { Express } from "express";
+
+import { registerMedicineRoutes } from "./medicines";
+import { registerCategoryRoutes } from "./categories";
+import { registerOrderRoutes } from "./orders";
+
+export function registerRoutes(app: Express) {
+  registerMedicineRoutes(app);
+  registerCategoryRoutes(app);
+  registerOrderRoutes(app);
+}
 
