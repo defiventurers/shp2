6aee715f894e96de5cbe4c21771fb641ecd179a7
defiventurers <140104441+defiventurers@users.noreply.github.com>
2025-12-21 06:42:59 +0530

    Update queryClient.ts

--- client/src/lib/queryClient.ts
+++ client/src/lib/queryClient.ts
@@ -27,33 +27,29 @@ export async function apiRequest(
 
 type UnauthorizedBehavior = "returnNull" | "throw";
 
-export const getQueryFn: <T>(options: {
-  on401: UnauthorizedBehavior;
-}) => QueryFunction<T> =
-  ({ on401: unauthorizedBehavior }) =>
+export const getQueryFn =
+  <T>({ on401 }: { on401: UnauthorizedBehavior }): QueryFunction<T> =>
   async ({ queryKey }) => {
-    const res = await fetch(
-      `${API_BASE_URL}${queryKey.join("/")}`,
-      {
-        credentials: "include",
-      }
-    );
+    const path = queryKey[0] as string;
 
-    if (unauthorizedBehavior === "returnNull" && res.status === 401) {
-      return null;
+    const res = await fetch(`${API_BASE_URL}${path}`, {
+      credentials: "include",
+    });
+
+    if (on401 === "returnNull" && res.status === 401) {
+      return null as T;
     }
 
     await throwIfResNotOk(res);
-    return await res.json();
+    return res.json();
   };
 
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       queryFn: getQueryFn({ on401: "throw" }),
-      refetchInterval: false,
       refetchOnWindowFocus: false,
-      staleTime: Infinity,
+      staleTime: 0,
       retry: false,
     },
     mutations: {
