543abff37bb3ad41e1bf4b98cc1f7bb295e8bf6f
defiventurers <140104441+defiventurers@users.noreply.github.com>
2026-02-03 01:23:52 +0530

    Delete server/scripts/runImport.ts

--- server/scripts/runImport.ts
+++
@@ -1,7 +0,0 @@
-import { importMedicinesFromCSV } from "./importMedicinesFromCSV";
-
-(async () => {
-  await importMedicinesFromCSV();
-  console.log("✅ Medicine import finished. Exiting.");
-  process.exit(0);
-})();
